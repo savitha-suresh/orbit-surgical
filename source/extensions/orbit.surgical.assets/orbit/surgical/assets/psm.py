3be14efcb877db940fefd9548b34a3647fed6a1e
Savitha Suresh <savithasuresh2410@gmail.com>
2025-07-15 18:03:28 +0800

    164 - changed the stiffness and damping in psm and revert obj cfg

--- source/extensions/orbit.surgical.assets/orbit/surgical/assets/psm.py
+++ source/extensions/orbit.surgical.assets/orbit/surgical/assets/psm.py
@@ -66,10 +66,10 @@ PSM_CFG = ArticulationCfg(
         ),
         "psm_tool": ImplicitActuatorCfg(
             joint_names_expr=["psm_tool_gripper.*"],
-            effort_limit=0.1,
+            effort_limit=50,
             velocity_limit=0.2,
-            stiffness=500,
-            damping=0.1,
+            stiffness=1000,
+            damping=80,
         ),
     },
     soft_joint_pos_limit_factor=1.0,
