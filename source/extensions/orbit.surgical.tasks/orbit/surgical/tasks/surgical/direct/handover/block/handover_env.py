f3e434458a29f2c76336fd2adf4e411b89734844
Savitha Suresh <savithasuresh2410@gmail.com>
2025-07-15 14:58:13 +0800

    160: to reach obj also the gripper should be facing down, added a param to make sure roll joint is also included while reaching obj

--- source/extensions/orbit.surgical.tasks/orbit/surgical/tasks/surgical/direct/handover/block/handover_env.py
+++ source/extensions/orbit.surgical.tasks/orbit/surgical/tasks/surgical/direct/handover/block/handover_env.py
@@ -326,7 +326,7 @@ class DualArmHandoverEnv(DirectMARLEnv):
         action_scale = torch.full((self.num_envs, 1), base_scale, device=self.device)
 
         # Check if GRIP_OPEN phase is active (1 or True)
-        grip_open_active = self.current_phases[:, Phases.REACH_OBJ_GRIP.value] | self.current_phases[:, Phases.GRIP_1_CLOSE.value]
+        grip_open_active = self.current_phases[:, Phases.REACH_OBJ_GRIP.value].bool() | self.current_phases[:, Phases.GRIP_1_CLOSE.value].bool()
 
         # Apply reduced scale where grip is open
         action_scale[grip_open_active] = reduced_scale
