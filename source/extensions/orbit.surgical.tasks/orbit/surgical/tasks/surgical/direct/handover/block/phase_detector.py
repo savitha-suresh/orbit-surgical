29fe8d6a08578d25d2fdce87b0ca201ce2cef8f3
Savitha Suresh <savithasuresh2410@gmail.com>
2025-07-31 15:55:20 +0800

    Removed the threshold check

--- source/extensions/orbit.surgical.tasks/orbit/surgical/tasks/surgical/direct/handover/block/phase_detector.py
+++ source/extensions/orbit.surgical.tasks/orbit/surgical/tasks/surgical/direct/handover/block/phase_detector.py
@@ -198,12 +198,13 @@ class PhaseDetector:
             
                 (  (
                     (grp1_tgt_dist <= self.GRIP_CLOSE_THRESHOLD) & 
-                    (grp2_tgt_dist <= self.GRIP_CLOSE_THRESHOLD)) |
-                    (
-                    (((grp1_tgt_dist < 0.008) & (prev_phases[:, Phases.GRIP_1_CLOSE.value])) & 
-                    ((grp2_tgt_dist < 0.008) & ((prev_phases[:, Phases.GRIP_1_CLOSE.value]))))
+                    (grp2_tgt_dist <= self.GRIP_CLOSE_THRESHOLD)) 
+                    # |
+                    # (
+                    # (((grp1_tgt_dist < 0.008) & (prev_phases[:, Phases.GRIP_1_CLOSE.value])) & 
+                    # ((grp2_tgt_dist < 0.008) & ((prev_phases[:, Phases.GRIP_1_CLOSE.value]))))
 
-                    )
+                    # )
                 )&
                 #(grip_link_tgt_dist <= self.GRIP_CLOSE_THRESHOLD) &
             (~obj_above_ground) &
@@ -224,9 +225,9 @@ class PhaseDetector:
         # # # PHASE 3: LIFT
         phase_mask[:, Phases.LIFT.value] = (
 
-            (
-                    (grp1_tgt_dist < 0.008) & 
-                    (grp2_tgt_dist < 0.008)) & 
+           # (
+                    # (grp1_tgt_dist < 0.008) & 
+                    # (grp2_tgt_dist < 0.008)) & 
             (~obj_above_ground) &
             (gripper_width < 0.15) & ~self.env.not_visited_mask[:, Phases.REACH_OBJ_GRIP.value]
         )
