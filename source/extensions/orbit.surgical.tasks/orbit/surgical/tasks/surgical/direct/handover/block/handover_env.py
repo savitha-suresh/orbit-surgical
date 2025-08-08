1223d0b7448aedc302deb3b773498ed42a4e0241
Savitha Suresh <savithasuresh2410@gmail.com>
2025-08-08 19:05:56 +0800

    position change

--- source/extensions/orbit.surgical.tasks/orbit/surgical/tasks/surgical/direct/handover/block/handover_env.py
+++ source/extensions/orbit.surgical.tasks/orbit/surgical/tasks/surgical/direct/handover/block/handover_env.py
@@ -563,7 +563,7 @@ class DualArmHandoverEnv(DirectMARLEnv):
         base_pos = self.object.data.root_pos_w         # (N, 3)
         base_rot = self.object.data.root_quat_w         # (N, 4)
         
-        local_offset = torch.tensor([[-0.00012, 0.005, 0.02]], device=base_pos.device)  # (1, 3)
+        local_offset = torch.tensor([[0.003, 0.01, 0.02]], device=base_pos.device)  # (1, 3)
         N = base_pos.shape[0]
         local_offset = local_offset.expand(N, -1) 
         rot_mat = quat_to_matrix(base_rot)             # (N, 3, 3)
@@ -579,7 +579,7 @@ class DualArmHandoverEnv(DirectMARLEnv):
         base_pos = self.object.data.root_pos_w         # (N, 3)
         base_rot = self.object.data.root_quat_w         # (N, 4)
         
-        local_offset = torch.tensor([[0.00012, -0.005, 0.02]], device=base_pos.device)  # (1, 3)
+        local_offset = torch.tensor([[-0.003, -0.01, 0.02]], device=base_pos.device)  # (1, 3)
         N = base_pos.shape[0]
         local_offset = local_offset.expand(N, -1) 
         rot_mat = quat_to_matrix(base_rot)             # (N, 3, 3)
@@ -633,7 +633,7 @@ class DualArmHandoverEnv(DirectMARLEnv):
         base_pos = self.object.data.root_pos_w          # (N, 3)
         base_rot = self.object.data.root_quat_w          # (N, 4)
         # x,y are for the needle
-        local_offset = torch.tensor([[-0.00012, 0.005, 0]], device=base_pos.device)  # (1, 3)
+        local_offset = torch.tensor([[0.003, 0.01, 0]], device=base_pos.device)  # (1, 3)
         N = base_pos.shape[0]
         local_offset = local_offset.expand(N, -1)
 
@@ -650,7 +650,7 @@ class DualArmHandoverEnv(DirectMARLEnv):
         base_pos = self.object.data.root_pos_w          # (N, 3)
         base_rot = self.object.data.root_quat_w          # (N, 4)
         # x,y are for the needle
-        local_offset = torch.tensor([[0.00012, -0.005, 0]], device=base_pos.device)  # (1, 3)
+        local_offset = torch.tensor([[0.003, -0.01, 0]], device=base_pos.device)  # (1, 3)
         N = base_pos.shape[0]
         local_offset = local_offset.expand(N, -1)
 
